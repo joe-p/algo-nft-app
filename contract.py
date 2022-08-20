ef8c1fff4778a225a5b7ada5900c60d412a97123
Joe Polny <joepolny@gmail.com>
2022-08-20 13:04:31 -0400

    initial beaker conversion

--- contract.py
+++ contract.py
@@ -2,6 +2,8 @@
 from pyteal import *
 from beaker import *
 import os
+import json
+from typing import Final
 
 ARGS = Txn.application_args
 
@@ -35,8 +37,22 @@ def get(key):
     return App.globalGet(key)
 
 class MyApp(Application):
-    @Subroutine(TealType.none)
-    def clawback_asa():
+    owner: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.bytes)
+    royalty_address: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.bytes, key=ROYALTY_ADDR)
+    highest_bidder: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.bytes, key=HIGHEST_BIDDER)
+    metadata: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.bytes)
+
+    auction_end: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, key=AUCTION_END)
+    sale_price: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, key=SALE_PRICE)
+    highest_bid: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, key=HIGHEST_BID)
+    royalty_percent: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, key=ROYALTY_PERCENT)
+    allow_transfer: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, key=ALLOW_TRANSFER)
+    allow_sale: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, key=ALLOW_SALE)
+    allow_auction: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, key=ALLOW_AUCTION)
+    asa_id: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.uint64, key=ASA_ID)
+
+    @internal(TealType.none)
+    def clawback_asa(self):
         asa_id = get(ASA_ID)
         highest_bidder = get(HIGHEST_BIDDER)
 
@@ -61,8 +77,8 @@ class MyApp(Application):
         return If(asa_id != Int(0), clawback_seq)
 
 
-    @Subroutine(TealType.none)
-    def claim_asa():
+    @external
+    def claim_asa(self):
         asa_id = get(ASA_ID)
         owner = get(OWNER)
 
@@ -81,36 +97,40 @@ class MyApp(Application):
         )
 
 
-    @Subroutine(TealType.none)
-    def init():
-        royalty_addr = ARGS[0]
-        royalty_percent = Btoi(ARGS[1])
-        metadata = ARGS[2]
-        allow_transfer = Btoi(ARGS[3])
-        allow_sale = Btoi(ARGS[4])
-        allow_auction = Btoi(ARGS[5])
-        asa_id = Btoi(ARGS[6])
+    @create
+    def create():
+        return Approve()
 
+    @external(authorize=Authorize.only(Global.creator_address()))
+    def initialize(self, 
+        royalty_addr: abi.Address, 
+        royalty_percent: abi.Uint64,
+        metadata: abi.String,
+        allow_transfer: abi.Bool,
+        allow_sale: abi.Bool,
+        allow_auction: abi.Bool,
+        asa_id: abi.Uint64
+    ):
         return Seq(
             # Set global bytes
-            set(ROYALTY_ADDR, royalty_addr),
+            set(ROYALTY_ADDR, royalty_addr.get()),
             set(OWNER, Txn.sender()),
             set(HIGHEST_BIDDER, ""),
-            set(METADATA, metadata),
+            set(METADATA, metadata.get()),
             # Set global ints
-            set(ROYALTY_PERCENT, royalty_percent),
+            set(ROYALTY_PERCENT, royalty_percent.get()),
             set(AUCTION_END, 0),
-            set(ALLOW_TRANSFER, allow_transfer),
-            set(ALLOW_SALE, allow_sale),
-            set(ALLOW_AUCTION, allow_auction),
+            set(ALLOW_TRANSFER, allow_transfer.get()),
+            set(ALLOW_SALE, allow_sale.get()),
+            set(ALLOW_AUCTION, allow_auction.get()),
             set(SALE_PRICE, 0),
             set(HIGHEST_BID, 0),
-            set(ASA_ID, asa_id),
+            set(ASA_ID, asa_id.get()),
         )
 
 
-    @Subroutine(TealType.none)
-    def buy():
+    @external
+    def buy(self):
         royalty_payment = Gtxn[Txn.group_index() + Int(2)]
         payment = Gtxn[Txn.group_index() + Int(1)]
 
@@ -136,14 +156,12 @@ class MyApp(Application):
             # Update global state
             set(OWNER, Txn.sender()),
             set(SALE_PRICE, 0),
-            clawback_asa(),
+            self.clawback_asa(),
         )
 
 
-    @Subroutine(TealType.none)
-    def start_sale():
-        price = Btoi(ARGS[1])
-
+    @external
+    def start_sale(self, price: abi.Uint64):
         allow_sale = get(ALLOW_SALE)
         owner = get(OWNER)
         auction_end = get(AUCTION_END)
@@ -152,21 +170,19 @@ class MyApp(Application):
             Assert(allow_sale),
             Assert(auction_end == Int(0)),
             Assert(Txn.sender() == owner),
-            set(SALE_PRICE, price),
+            set(SALE_PRICE, price.get()),
         )
 
 
-    @Subroutine(TealType.none)
+    @external
     def end_sale():
         owner = get(OWNER)
 
         return Seq(Assert(Txn.sender() == owner), set(SALE_PRICE, 0))
 
 
-    @Subroutine(TealType.none)
-    def transfer():
-        receiver = ARGS[1]
-
+    @external
+    def transfer(self, receiver: abi.Address):
         allow_transfer = get(ALLOW_TRANSFER)
         owner = get(OWNER)
         auction_end = get(AUCTION_END)
@@ -175,17 +191,14 @@ class MyApp(Application):
             Assert(allow_transfer),
             Assert(auction_end == Int(0)),
             Assert(Txn.sender() == owner),
-            set(OWNER, receiver),
-            clawback_asa(),
+            set(OWNER, receiver.get()),
+            self.clawback_asa(),
         )
 
 
-    @Subroutine(TealType.none)
-    def start_auction():
+    @external
+    def start_auction(self, starting_price: abi.Uint64, length: abi.Uint64):
         payment = Gtxn[Txn.group_index() + Int(1)]
-
-        starting_price = Btoi(ARGS[1])
-        length = Btoi(ARGS[2])
 
         allow_auction = get(ALLOW_AUCTION)
 
@@ -195,13 +208,13 @@ class MyApp(Application):
             Assert(payment.receiver() == Global.current_application_address()),
             Assert(payment.amount() == Int(100_000)),
             # Set global state
-            set(AUCTION_END, Global.latest_timestamp() + length),
-            set(HIGHEST_BID, starting_price),
+            set(AUCTION_END, Global.latest_timestamp() + length.get()),
+            set(HIGHEST_BID, starting_price.get()),
         )
 
 
-    @Subroutine(TealType.none)
-    def pay(receiver, amount):
+    @internal(TealType.none)
+    def pay(self, receiver: Expr, amount: Expr):
         return Seq(
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields(
@@ -215,8 +228,8 @@ class MyApp(Application):
         )
 
 
-    @Subroutine(TealType.none)
-    def end_auction():
+    @external
+    def end_auction(self):
         auction_end = get(AUCTION_END)
         highest_bid = get(HIGHEST_BID)
         royalty_percent = get(ROYALTY_PERCENT)
@@ -228,18 +241,18 @@ class MyApp(Application):
         return Seq(
             Assert(Global.latest_timestamp() > auction_end),
             # Pay royalty address and owner
-            pay(royalty_address, royalty_amount),
-            pay(owner, highest_bid - royalty_amount),
+            self.pay(royalty_address, royalty_amount),
+            self.pay(owner, highest_bid - royalty_amount),
             # Set global state
             set(AUCTION_END, 0),
             set(OWNER, highest_bidder),
             set(HIGHEST_BIDDER, ""),
-            clawback_asa(),
+            self.clawback_asa(),
         )
 
 
-    @Subroutine(TealType.none)
-    def bid():
+    @external
+    def bid(self):
         payment = Gtxn[Txn.group_index() + Int(1)]
 
         auction_end = get(AUCTION_END)
@@ -252,52 +265,52 @@ class MyApp(Application):
             Assert(payment.amount() > highest_bid),
             Assert(Txn.sender() == payment.sender()),
             # Return previous bid
-            If(highest_bidder != Bytes(""), pay(highest_bidder, highest_bid)),
+            If(highest_bidder != Bytes(""), self.pay(highest_bidder, highest_bid)),
             # Set global state
             set(HIGHEST_BID, payment.amount()),
             set(HIGHEST_BIDDER, payment.sender()),
         )
 
 
-    def approval():
-        fcn = ARGS[0]
-
-        return Seq(
-            Cond(
-                [Txn.application_id() == Int(0), init()],
-                # Delete only for debugging sake
-                # TODO: Implement delete function that requires input from owner and creator
-                [Txn.on_completion() == OnComplete.DeleteApplication, Approve()],
-                [fcn == Bytes("start_auction"), start_auction()],
-                [fcn == Bytes("start_sale"), start_sale()],
-                [fcn == Bytes("end_sale"), end_sale()],
-                [fcn == Bytes("bid"), bid()],
-                [fcn == Bytes("end_auction"), end_auction()],
-                [fcn == Bytes("transfer"), transfer()],
-                [fcn == Bytes("buy"), buy()],
-                [fcn == Bytes("claim_asa"), claim_asa()],
-            ),
-            Approve(),
-        )
-
-
-    def clear():
-        return Approve()
-
-
 if __name__ == "__main__":
+    app = MyApp()
+
     if os.path.exists("approval.teal"):
         os.remove("approval.teal")
 
     if os.path.exists("approval.teal"):
         os.remove("clear.teal")
 
-    compiled_approval = compileTeal(approval(), mode=Mode.Application, version=5)
+    if os.path.exists("abi.json"):
+        os.remove("abi.json")
 
     with open("approval.teal", "w") as f:
-        f.write(compiled_approval)
-
-    compiled_clear = compileTeal(clear(), mode=Mode.Application, version=5)
+        f.write(app.approval_program)
 
     with open("clear.teal", "w") as f:
-        f.write(compiled_clear)
+        f.write(app.clear_program)
+
+    with open("abi.json", "w") as f:
+        f.write(json.dumps(app.contract.dictify(), indent=4))
+
+acct = sandbox.get_accounts().pop()
+
+app_client = client.ApplicationClient(
+    client=sandbox.get_algod_client(), 
+    app=MyApp(version=6), 
+    signer=acct.signer
+)
+
+app_client.create()
+app_client.call(
+    MyApp.initialize,
+    royalty_addr = acct.address, 
+    royalty_percent = 10,
+    metadata = "Hello World",
+    allow_transfer = True,
+    allow_sale = True,
+    allow_auction = True,
+    asa_id = 0
+    )
+
+print(app_client.get_application_state())
