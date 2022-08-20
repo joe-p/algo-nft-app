16c4c212d514efd47aa62e8630218710f34295c6
Joe Polny <joepolny@gmail.com>
2022-08-20 13:20:30 -0400

    use beaker state

--- contract.py
+++ contract.py
@@ -23,19 +23,6 @@ ALLOW_SALE = Bytes("allowSale")
 ALLOW_AUCTION = Bytes("allowAuction")
 ASA_ID = Bytes("asaID")
 
-
-def set(key, value):
-    if type(value) == str:
-        value = Bytes(value)
-    elif type(value) == int:
-        value = Int(value)
-
-    return App.globalPut(key, value)
-
-
-def get(key):
-    return App.globalGet(key)
-
 class MyApp(Application):
     owner: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.bytes)
     royalty_address: Final[ApplicationStateValue] = ApplicationStateValue(stack_type=TealType.bytes, key=ROYALTY_ADDR)
@@ -53,8 +40,8 @@ class MyApp(Application):
 
     @internal(TealType.none)
     def clawback_asa(self):
-        asa_id = get(ASA_ID)
-        highest_bidder = get(HIGHEST_BIDDER)
+        asa_id = self.asa_id.get()
+        highest_bidder = self.highest_bidder.get()
 
         clawback_txn = Seq(
             InnerTxnBuilder.Begin(),
@@ -79,8 +66,8 @@ class MyApp(Application):
 
     @external
     def claim_asa(self):
-        asa_id = get(ASA_ID)
-        owner = get(OWNER)
+        asa_id = self.asa_id.get()
+        owner = self.owner.get()
 
         return Seq(
             Assert(asa_id),
@@ -112,20 +99,18 @@ class MyApp(Application):
         asa_id: abi.Uint64
     ):
         return Seq(
-            # Set global bytes
-            set(ROYALTY_ADDR, royalty_addr.get()),
-            set(OWNER, Txn.sender()),
-            set(HIGHEST_BIDDER, ""),
-            set(METADATA, metadata.get()),
-            # Set global ints
-            set(ROYALTY_PERCENT, royalty_percent.get()),
-            set(AUCTION_END, 0),
-            set(ALLOW_TRANSFER, allow_transfer.get()),
-            set(ALLOW_SALE, allow_sale.get()),
-            set(ALLOW_AUCTION, allow_auction.get()),
-            set(SALE_PRICE, 0),
-            set(HIGHEST_BID, 0),
-            set(ASA_ID, asa_id.get()),
+            self.royalty_address.set(royalty_addr.get()),
+            self.owner.set(Txn.sender()),
+            self.highest_bidder.set(Bytes("")),
+            self.metadata.set(metadata.get()),
+            self.royalty_percent.set(royalty_percent.get()),
+            self.auction_end.set(Int(0)),
+            self.allow_transfer.set(allow_transfer.get()),
+            self.allow_sale.set(allow_sale.get()),
+            self.allow_auction.set(allow_auction.get()),
+            self.sale_price.set(Int(0)),
+            self.highest_bid.set(Int(0)),
+            self.asa_id.set(asa_id.get()),
         )
 
 
@@ -134,10 +119,10 @@ class MyApp(Application):
         royalty_payment = Gtxn[Txn.group_index() + Int(2)]
         payment = Gtxn[Txn.group_index() + Int(1)]
 
-        sale_price = get(SALE_PRICE)
-        royalty_percent = get(ROYALTY_PERCENT)
-        royalty_address = get(ROYALTY_ADDR)
-        owner = get(OWNER)
+        sale_price = self.sale_price.get()
+        royalty_percent = self.royalty_percent.get()
+        royalty_address = self.royalty_address.get()
+        owner = self.owner.get()
 
         royalty_amt = sale_price * royalty_percent / Int(100)
         purchase_amt = sale_price - royalty_amt
@@ -154,44 +139,44 @@ class MyApp(Application):
             Assert(royalty_payment.amount() == royalty_amt),
             Assert(payment.amount() == purchase_amt),
             # Update global state
-            set(OWNER, Txn.sender()),
-            set(SALE_PRICE, 0),
+            self.owner.set(Txn.sender()),
+            self.sale_price.set(Int(0)),
             self.clawback_asa(),
         )
 
 
     @external
     def start_sale(self, price: abi.Uint64):
-        allow_sale = get(ALLOW_SALE)
-        owner = get(OWNER)
-        auction_end = get(AUCTION_END)
+        allow_sale = self.allow_sale.get()
+        owner = self.owner.get()
+        auction_end = self.auction_end.get()
 
         return Seq(
             Assert(allow_sale),
             Assert(auction_end == Int(0)),
             Assert(Txn.sender() == owner),
-            set(SALE_PRICE, price.get()),
+            self.sale_price.set(price.get()),
         )
 
 
     @external
-    def end_sale():
-        owner = get(OWNER)
+    def end_sale(self):
+        owner = self.owner.get()
 
-        return Seq(Assert(Txn.sender() == owner), set(SALE_PRICE, 0))
+        return Seq(Assert(Txn.sender() == owner), self.sale_price.set(Int(0)))
 
 
     @external
     def transfer(self, receiver: abi.Address):
-        allow_transfer = get(ALLOW_TRANSFER)
-        owner = get(OWNER)
-        auction_end = get(AUCTION_END)
+        allow_transfer = self.allow_transfer.get()
+        owner = self.owner.get()
+        auction_end = self.auction_end.get()
 
         return Seq(
             Assert(allow_transfer),
             Assert(auction_end == Int(0)),
             Assert(Txn.sender() == owner),
-            set(OWNER, receiver.get()),
+            self.owner.set(receiver.get()),
             self.clawback_asa(),
         )
 
@@ -200,7 +185,7 @@ class MyApp(Application):
     def start_auction(self, starting_price: abi.Uint64, length: abi.Uint64):
         payment = Gtxn[Txn.group_index() + Int(1)]
 
-        allow_auction = get(ALLOW_AUCTION)
+        allow_auction = self.allow_auction.get()
 
         return Seq(
             Assert(allow_auction),
@@ -208,8 +193,8 @@ class MyApp(Application):
             Assert(payment.receiver() == Global.current_application_address()),
             Assert(payment.amount() == Int(100_000)),
             # Set global state
-            set(AUCTION_END, Global.latest_timestamp() + length.get()),
-            set(HIGHEST_BID, starting_price.get()),
+            self.auction_end.set(Global.latest_timestamp() + length.get()),
+            self.highest_bid.set(starting_price.get()),
         )
 
 
@@ -230,13 +215,13 @@ class MyApp(Application):
 
     @external
     def end_auction(self):
-        auction_end = get(AUCTION_END)
-        highest_bid = get(HIGHEST_BID)
-        royalty_percent = get(ROYALTY_PERCENT)
+        auction_end = self.auction_end.get()
+        highest_bid = self.highest_bid.get()
+        royalty_percent = self.royalty_percent.get()
         royalty_amount = highest_bid * royalty_percent / Int(100)
-        royalty_address = get(ROYALTY_ADDR)
-        owner = get(OWNER)
-        highest_bidder = get(HIGHEST_BIDDER)
+        royalty_address = self.royalty_address.get()
+        owner = self.owner.get()
+        highest_bidder = self.highest_bidder.get()
 
         return Seq(
             Assert(Global.latest_timestamp() > auction_end),
@@ -244,9 +229,9 @@ class MyApp(Application):
             self.pay(royalty_address, royalty_amount),
             self.pay(owner, highest_bid - royalty_amount),
             # Set global state
-            set(AUCTION_END, 0),
-            set(OWNER, highest_bidder),
-            set(HIGHEST_BIDDER, ""),
+            self.auction_end.set(Int(0)),
+            self.owner.set(highest_bidder),
+            self.highest_bidder.set(Bytes("")),
             self.clawback_asa(),
         )
 
@@ -255,9 +240,9 @@ class MyApp(Application):
     def bid(self):
         payment = Gtxn[Txn.group_index() + Int(1)]
 
-        auction_end = get(AUCTION_END)
-        highest_bidder = get(HIGHEST_BIDDER)
-        highest_bid = get(HIGHEST_BID)
+        auction_end = self.auction_end.get()
+        highest_bidder = self.highest_bidder.get()
+        highest_bid = self.highest_bid.get()
 
         return Seq(
             Assert(Global.latest_timestamp() < auction_end),
@@ -267,8 +252,8 @@ class MyApp(Application):
             # Return previous bid
             If(highest_bidder != Bytes(""), self.pay(highest_bidder, highest_bid)),
             # Set global state
-            set(HIGHEST_BID, payment.amount()),
-            set(HIGHEST_BIDDER, payment.sender()),
+            self.highest_bid.set(payment.amount()),
+            self.highest_bidder.set(payment.sender()),
         )
 
 
